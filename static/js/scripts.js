e3910fe8c3b1dcd7c78df5073868435ba45b8898
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-12-19 17:09:19 +0100

    refactor: extract dataStream logic

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,4 +1,3 @@
-let eventSources = {};
 let patientCounter = 0;
 let globalRefreshRate = 1000;
 
@@ -17,63 +16,6 @@ function setGlobalRefreshRate(rate) {
             startPatientStream(patientNum);
         }
     });
-}
-
-function startPatientStream(patientNum) {
-    stopPatientStream(patientNum);
-
-    const panel = $(`#patient-panel-${patientNum}`);
-    const patientId = panel.find('.patient-id').val();
-    const activityId = panel.find('.activity-id').val();
-    const refreshRate = panel.find('.refresh-rate').val();
-
-    const eventSource = new EventSource(`/data/${patientId}/${activityId}/${refreshRate}`);
-    eventSources[patientNum] = eventSource;
-
-    eventSource.onmessage = function(event) {
-        const data = event.data.split(',');
-        const selectedSensors = panel.find('.sensor-checkbox:checked').map(function() {
-            return $(this).val();
-        }).get();
-
-        let filteredData = data.filter((value, index) => selectedSensors.includes(Object.keys(sensorTypes)[index]));
-
-        if (filteredData.length > 0) {
-            const formattedData = `Patient ${patientId}: ${filteredData.join(', ')}`;
-            $('#common-data-container').append(`<p>${formattedData}</p>`);
-
-            // Limiter le nombre de lignes affichées dans le conteneur de texte
-            const maxTextLines = 100;
-            const textContainer = $('#common-data-container');
-            if (textContainer.children().length > maxTextLines) {
-                textContainer.children().slice(0, textContainer.children().length - maxTextLines).remove();
-            }
-
-            textContainer.scrollTop(textContainer[0].scrollHeight);
-
-            updateChart(patientId, filteredData, selectedSensors);
-        }
-    };
-}
-
-function stopPatientStream(patientNum) {
-    if (eventSources[patientNum]) {
-        eventSources[patientNum].close();
-        delete eventSources[patientNum];
-    }
-}
-
-function startAllStreams() {
-    $('.patient-container').each(function() {
-        const patientNum = $(this).attr('id').replace('patient-panel-', '');
-        startPatientStream(parseInt(patientNum));
-    });
-}
-
-function stopAllStreams() {
-    for (let patientNum in eventSources) {
-        stopPatientStream(patientNum);
-    }
 }
 
 
