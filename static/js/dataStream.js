8fe91bd3939741ebe4efa5120c8c06a4047c592a
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-12-19 20:07:43 +0100

    feat: success message if connection reestablished

--- static/js/dataStream.js
+++ static/js/dataStream.js
@@ -1,12 +1,49 @@
 let eventSources = {};
 let reconnectAttempts = {};
+let isConnectionLost = false;
 const MAX_RECONNECT_ATTEMPTS = 5;
 const RECONNECT_DELAY = 5000;
 
+function checkServerConnection() {
+  $.ajax({
+    url: '/check_connection',
+    method: 'GET',
+    timeout: 5000,
+    success: function(response) {
+      console.log(`Serveur en fonctionnement (Server Time: ${response.timestamp})`);
+      if (isConnectionLost) {
+        displaySuccess("Connexion rétablie");
+        isConnectionLost = false;
+        startAllStreams(); // Redémarrer tous les flux de données
+      }
+    },
+    error: function(xhr, status, error) {
+      console.error(`Impossible d'établir une connexion avec le serveur (Local Time: ${new Date(Date.now()).toISOString()})`, error);
+      if (!isConnectionLost) {
+        displayError(`Impossible d'établir une connexion avec le serveur (Local Time: ${new Date(Date.now()).toISOString()})`);
+        isConnectionLost = true;
+        stopAllStreams();
+      }
+      setTimeout(checkServerConnection, RECONNECT_DELAY);
+    }
+  });
+}
+
 function displayError(message) {
   // Afficher une alerte en haut de la page
-  $('#error-alert').remove(); // Supprimer l'alerte précédente s'il y en a une
-  $('body').prepend(`<div id="error-alert" class="alert alert-danger alert-dismissible fade show" role="alert">
+  $('#banner-alert').remove(); // Supprimer l'alerte précédente s'il y en a une
+  $('body').prepend(`<div id="banner-alert" class="alert alert-danger alert-dismissible fade show" role="alert">
+        ${message}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>`);
+}
+
+function displaySuccess(message) {
+  // Supprimer l'alerte précédente s'il y en a une
+  $('#banner-alert').remove();
+
+  // Afficher une alerte de succès en haut de la page
+  $('body').prepend(`<div id="banner-alert" class="alert alert-success alert-dismissible fade show" role="alert">
         ${message}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>`);
@@ -91,21 +128,4 @@ function stopAllStreams() {
   for (let patientNum in eventSources) {
     stopPatientStream(patientNum);
   }
-}
-
-function checkServerConnection() {
-  $.ajax({
-    url: '/check_connection',
-    method: 'GET',
-    timeout: 5000,
-    success: function(response) {
-      console.log(`Serveur en fonctionnement (Server Time: ${response.timestamp})`);
-    },
-    error: function(xhr, status, error) {
-      console.error(`Impossible d'établir une connexion avec le serveur (Local Time: ${new Date(Date.now()).toISOString()})`, error);
-      displayError(`Impossible d'établir une connexion avec le serveur (Local Time: ${new Date(Date.now()).toISOString()})`);
-      stopAllStreams();
-      setTimeout(checkServerConnection, RECONNECT_DELAY);
-    }
-  });
 }
